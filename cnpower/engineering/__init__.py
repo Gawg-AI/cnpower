a98f5fb98c7595f3149329412ca2d3ed3dbe2c69
GAWG-AI <fejj258@gmail.com>
2026-06-03 18:59:38 +0800

    feat: export engineering normalization and builder APIs

--- cnpower/engineering/__init__.py
+++ cnpower/engineering/__init__.py
@@ -1,6 +1,8 @@
 from .asset_schema import get_engineering_asset_schema
-from .compliance_checker import check_basic_equipment_compliance
+from .compliance_checker import check_basic_equipment_compliance, check_equipment_compliance
 from .compliance_constraints import get_compliance_constraint_library
+from .network_builder import build_pandapower_net, create_pandapower_net
+from .normalization import normalize_equipment, normalize_results, canonical_equipment_type
 from .planning_library import get_planning_assumption_library
 from .pandapower_bridge import get_pandapower_bridge_spec
 
